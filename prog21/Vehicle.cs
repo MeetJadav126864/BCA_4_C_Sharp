e3a3f5d27b8663734b2c5435ee2917307a925b5e
Meet Dinesh Jadav <meetjadav115@gmail.com>
2026-03-24 08:32:40 +0530

    prog21

---
+++ prog21/Vehicle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prog21
+{
+    internal class Vehicle
+    {
+      string vehicle_type;
+
+        public void get_data()
+        {
+            Console.WriteLine("Enter vehicle type");
+            vehicle_type = Console.ReadLine();
+        }
+
+        public void show_vehicle()
+        {
+            Console.WriteLine("Vehicle type is: " + vehicle_type);
+        }
+        
+
+    }
+}
---
+++ prog21/car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace prog21
+{
+    internal class car:Vehicle
+    {
+        string model_type, company_name;
+
+        public void get_car()
+        {
+            Console.WriteLine("Enter model type");
+            model_type = Console.ReadLine();
+            Console.WriteLine("Enter company name");
+            company_name = Console.ReadLine();
+        }
+
+        public void show_car()
+        {
+            Console.WriteLine("Model type is: " + model_type);
+            Console.WriteLine("Company name is: " + company_name);
+        }
+
+
+    }
+}
